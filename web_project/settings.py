5655e92c62589f42e428660fc2070e8fceca411f
Jaeyong Kang <kjysmu@gmail.com>
2025-08-11 21:59:14 +0800

    Update settings.py

--- web_project/settings.py
+++ web_project/settings.py
@@ -18,9 +18,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-vu@ah45w%25@=e3v8(e190$ln7$1q$6t9jkk)lmrdy7&$7khm5"
+SECRET_KEY = "XXX"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -51,23 +50,6 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = "web_project.urls"
-
-# TEMPLATES = [
-#     {
-#         "BACKEND": "django.template.backends.django.DjangoTemplates",
-#         "DIRS": [],
-#         "APP_DIRS": True,
-#         "OPTIONS": {
-#             "context_processors": [
-#                 "django.template.context_processors.debug",
-#                 "django.template.context_processors.request",
-#                 "django.contrib.auth.context_processors.auth",
-#                 "django.contrib.messages.context_processors.messages",
-#             ],
-#         },
-#     },
-# ]
-
 
 TEMPLATES = [
     {
